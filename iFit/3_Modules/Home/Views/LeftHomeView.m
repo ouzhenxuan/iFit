cb37dba53f33a04b02d15d0a0b78febddf70316b
ozx <442910407@qq.com>
2017-10-31 09:49:52 +0800

    添加了左侧滑动控件的foot header view

--- iFit/3_Modules/Home/Views/LeftHomeView.m
+++ iFit/3_Modules/Home/Views/LeftHomeView.m
@@ -10,6 +10,8 @@
 
 @interface LeftHomeView() <UITableViewDelegate,UITableViewDataSource>
 
+@property (nonatomic,strong) UITableView * tableview;
+
 @end
 
 @implementation LeftHomeView
@@ -29,17 +31,29 @@
     if (self) {
         self.backgroundColor = [UIColor yellowColor];
         
-        UITableView * tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 40, self.bounds.size.width, self.bounds.size.height-40) style:UITableViewStylePlain];
+        _tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 40, self.bounds.size.width, self.bounds.size.height-40) style:UITableViewStylePlain];
         
-        tableview.backgroundColor = [UIColor greenColor];
-        [self addSubview:tableview];
+        _tableview.backgroundColor = [UIColor greenColor];
+        [self addSubview:_tableview];
         
-        tableview.dataSource = self;
-        tableview.delegate = self;
+        _tableview.dataSource = self;
+        _tableview.delegate = self;
+        
         
     }
     return self;
 }
+
+- (void)setUpTheTableVie{
+    UIView * footView = [[UIView alloc ] initWithFrame:CGRectMake(0, 0, 1, 1)];
+    _tableview.tableFooterView = footView;
+    
+    UIView * headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenBoundsWidth, 210)];
+    headerView.backgroundColor = [UIColor yellowColor];
+    _tableview.tableHeaderView = headerView;
+    
+}
+
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
